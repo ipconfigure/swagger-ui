39a3cfe307fb603f61dee816fa97e48fa1c7d5d1
Evan Callaway <evan.callaway@ipconfigure.com>
2017-12-11 14:55:41 -0500

    Hide topbar

--- src/plugins/topbar/topbar.jsx
+++ src/plugins/topbar/topbar.jsx
@@ -130,7 +130,7 @@ export default class Topbar extends React.Component {
           <div className="topbar-wrapper">
             <Link href="/" title="@@APP_NAME@@ API">
               <img height="36" width="36" src="./logo_small.png" alt="@@APP_NAME@@ API" />
-              <span>@@APP_NAME@@</span>
+              <span>Orchid</span>
             </Link>
           </div>
         </div>
