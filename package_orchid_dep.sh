96f9c29b0d632054b2ad216222336ee5996bc879
Jake Foytik <jake.foytik@ipconfigure.com>
2017-12-14 15:29:10 -0500

    Remove branding from index.html. Update package script.

--- package_orchid_dep.sh
+++ package_orchid_dep.sh
@@ -6,9 +6,6 @@ GIT_TAG=$(git describe --tags)
 # Copy dist directory to staging directory
 cp -R dist $STAGE_DIR
 
-# Rename the template file
-mv ${STAGE_DIR}/index.html ${STAGE_DIR}/index.html.template
-
 # Create the package
 tar -czvf orchid-swagger-ui-dist-${GIT_TAG}.tar.gz ${STAGE_DIR}
 
